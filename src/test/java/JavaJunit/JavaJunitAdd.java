50f6586ba4b5225fbf81adb9c6b904b6113f1838
Premnath <vijayanand.premnath@worldline.com>
2022-08-10 17:17:53 +0530

    initial commit

---
+++ src/test/java/JavaJunit/JavaJunitAdd.java
@@ -0,0 +1,8 @@
+package JavaJunit;
+
+public class JavaJunitAdd {
+	public int add(int a,int b) {
+		return a+b;
+	}
+
+}
---
+++ src/test/java/JavaJunit/JavaJunitAddTest.java
@@ -0,0 +1,14 @@
+package JavaJunit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JavaJunitAddTest {
+
+	@Test
+	public void testAdd() {
+		
+	}
+
+}
